33277589231abee8299b323bc758fd1943f788de
Jwz <waiwai0308@gmail.com>
2017-05-06 13:42:42 +0800

    finished hw4

---
+++ answers/hw4/js/all.js
@@ -0,0 +1,38 @@
+var channelHTML = '';
+var row = document.querySelector(".row");
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://api.twitch.tv/kraken/streams/?game=League%20of%20Legends&limit=20", false);
+xhr.setRequestHeader('client-ID','4txreasawj2i2mgko04qtko6bnenfd');
+xhr.send();
+var channeldata = JSON.parse(xhr.responseText)
+
+function showdata(){
+	for(var i = 0; i < channeldata.streams.length; i++){
+		var div = document.createElement("div");
+		channelHTML = '';
+		channelHTML += '<div class="col">';
+		channelHTML += '	<div class="channel-video">';
+		channelHTML += '		<img src="'+channeldata.streams[i].preview.medium+'" alt=""></img>';
+		channelHTML += '	</div>';
+		channelHTML += '	<div class="channel-info">';
+		channelHTML += '		<div class="avatar">';
+		channelHTML += '			<img src="'+channeldata.streams[i].channel.logo+'" alt=""></img>';
+		channelHTML += '		</div>';
+		channelHTML += '		<div class="intro">';
+		channelHTML += '			<div class="channel_name">'+channeldata.streams[i].channel.status+'</div>';
+		channelHTML += '			<div class="owner_name">'+channeldata.streams[i].channel.name+'</div>';
+		channelHTML += '		</div>';
+		channelHTML += '	</div>';
+		channelHTML += '</div>';
+		div.innerHTML = channelHTML;
+		row.append(div);
+	}
+	var divNull = document.createElement('div');
+	divNull.className = "col cover";
+	row.append(divNull);
+	row.append(divNull);
+}
+
+
+showdata();
